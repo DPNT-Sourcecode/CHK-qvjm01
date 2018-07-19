ef9c637ef6e66fb2f9660e8ad521ac53a38aa84b
sbx_user1060 <sbx_user1060@ip-10-11-150-84.eu-west-2.compute.internal>
2018-07-19 14:32:06 +0000

    Thu Jul 19 14:32:06 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
 
-
+from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+PRICE = {
+    'A': (50, 3, 130),
+    'B': (30, 2, 45),
+    'C': (20, None, None),
+    'D': (15, None, None),
+}
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    products = Counter(skus)
+    price = 0
+    for product, count in products.items():
+        if products in PRICE:
+            single, prom, prom_price = PRICE[product]
+            if prom:
+                price += single * (count % prom) + prom_price * (count // 3)
+            else:
+                price += single * count
+        else:
+            return -1
+    return price
---
+++ test/solution_tests/test_checkout_r1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from solution.CHK.checkout_solution import checkout
+
+
+class TestCheckout(unittest.TestCase):
+    def test_checkout(self):
+        test_vector = [
+            ('A', 50),
+            ('F', -1),
+            ('AAAAA', 230),
+            ('AAAAAA', 260),
+            ('AAABB', 175),
+            ('AABCD', 165)
+        ]
