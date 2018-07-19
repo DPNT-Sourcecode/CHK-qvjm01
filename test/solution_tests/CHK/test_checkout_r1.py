317e08bfc2497ef8c43f0e6036ee189442cf03a0
sbx_user1060 <sbx_user1060@ip-10-11-150-84.eu-west-2.compute.internal>
2018-07-19 15:20:28 +0000

    Thu Jul 19 15:20:28 UTC 2018

--- test/solution_tests/CHK/test_checkout_r1.py
+++ test/solution_tests/CHK/test_checkout_r1.py
@@ -8,7 +8,7 @@ class TestCheckout(unittest.TestCase):
     def test_checkout(self):
         test_vector = [
             ('A', 50),
-            ('Z', -1),
+            ('123', -1),
             ('AAAA', 180),
             ('AAAAA', 200),
             ('AAAAAA', 250),
@@ -16,7 +16,8 @@ class TestCheckout(unittest.TestCase):
             ('EEEEEBB', 200),
             ('AAABB', 175),
             ('AABCD', 165),
-            ('F' * 6, 40)
+            ('F' * 6, 40),
+            ('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 965)
         ]
         for skus, expected in test_vector:
             self.assertEqual(checkout(skus), expected)
