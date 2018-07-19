e9aec4003622f408b3c81ae36cb437c9a2d29e33
sbx_user1060 <sbx_user1060@ip-10-11-150-84.eu-west-2.compute.internal>
2018-07-19 14:16:42 +0000

    Thu Jul 19 14:16:42 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,6 +4,6 @@
 # friend_name = unicode string
 def hello(friend_name):
     if friend_name:
-        return "Hello, {}".format(friend_name)
+        return "Hello, {}!".format(friend_name)
     else:
         return "Hello, World!"
